3a8cd2e099aae1097c1b33aa01a1ce53e6628c47
Shashi Bhagat <skujur871@gmail.com>
2022-11-29 22:34:11 +0530

    commit against new changes and completed histrogram

--- static/js/friday25.js
+++ static/js/friday25.js
@@ -57,8 +57,8 @@ logScaleExample();
 const histogram = async () => {
   // Data
   const dataset = await d3.json("static/files/data5.json");
-  const xAccessor = d => d.currently.humidity
-  const yAccessor = d => d.currently 
+  const xAccessor = (d) => d.currently.humidity;
+  const yAccessor = (d) => d.length;
 
   // Dimensions
   const dimensions = {
@@ -84,29 +84,51 @@ const histogram = async () => {
       `translate(${dimensions.margine}, ${dimensions.margine})`
     );
 
-  const xScale = d3.scaleLinear()
-        .domain(d3.extent(dataset, xAccessor))
-        .range([0, dimensions.ctrWidth])
-        .nice()
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.ctrWidth])
+    .nice();
 
-    const bin = d3.bin()
-        .domain(xScale.domain())
-        .value(xAccessor)
-        .thresholds(10)
-    
-    const newDataset = bin(dataset)
-    console.log(newDataset)
-    const padding = 1
+  const bin = d3.bin().domain(xScale.domain()).value(xAccessor).thresholds(10);
+  const newDataset = bin(dataset);
 
-    // Draw Bars
-    ctr.selectAll('rect')
-        .data(newDataset)
-        .join('rect')
-        .attr('width', d => d3.max([0, xScale(d.x0) - padding]))
-        .attr('height', 100)
-        .attr('x', d => xScale(d.x0))
-        .attr('y', 0)
-    
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(newDataset, yAccessor)])
+    .range([dimensions.ctrHeight, 0])
+    .nice();
+
+  const padding = 1;
+
+  // Draw Bars
+  ctr
+    .selectAll("rect")
+    .data(newDataset)
+    .join("rect")
+    .attr("width", (d) => d3.max([0, xScale(d.x1) - xScale(d.x0) - padding]))
+    .attr("height", (d) => dimensions.ctrHeight - yScale(yAccessor(d)))
+    .attr("x", (d) => xScale(d.x0))
+    .attr("y", (d) => yScale(yAccessor(d)))
+    .attr("fill", "#01c5c4");
+
+  ctr
+    .append("g")
+    .classed("bar-labels", true)
+    .selectAll("text")
+    .data(newDataset)
+    .join("text")
+    .attr("x", (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+    .attr("y", (d) => yScale(yAccessor(d)) - 10)
+    .text(yAccessor);
+
+  // Draw axis
+  const xAxis = d3.axisBottom(xScale);
+
+  const xAxisGroup = ctr
+    .append("g")
+    .style("transform", `translateY(${dimensions.ctrHeight}px)`)
+    .call(xAxis);
 };
 
 histogram();
