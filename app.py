28e22923cb90e602d1880e9126053eae03e4a4b2
Shashi Bhagat <skujur871@gmail.com>
2022-12-06 12:48:31 +0530

    new apis

--- app.py
+++ app.py
@@ -156,7 +156,6 @@ def learning_flask_with_python():
         def wrapper(*args):
             return function(*args) + 200
         return wrapper
-        
 
     @modify
     def add(a, b):
@@ -166,6 +165,46 @@ def learning_flask_with_python():
     student.return_new()
 
     return student.full_name()
+
+
+stores = [
+    {
+        "id": 1,
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+
+@app.get("/store")
+def get_store():
+    return {"stores": stores}
+
+
+@app.put('/store/<int:id>')
+def add_store(id: int):
+    print(stores.index(id))
+    for store in stores:
+        if store['id'] == id:
+            store["items"].append({
+                "name": request.json["name"],
+                "price": request.json["price"]
+            })
+            return stores, 201
+    return {'message': 'Store not found'}, 404
+
+
+@app.get('/store/<int:id>/items')
+def get_store_items(id: int):
+    for store in stores:
+        if store.get('id') == id:
+            return {'items': store.get('items')}, 201
+    return {'message': f'Store with id:{id} Not Found'}, 404
 
 
 if __name__ == "__main__":
