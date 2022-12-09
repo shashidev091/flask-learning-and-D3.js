45725014548ab8f6f69af6be3c276bff93f9c947
Shashi Bhagat <skujur871@gmail.com>
2022-12-09 22:27:29 +0530

    updated sqlAlchemy setup

--- models/store.py
+++ models/store.py
@@ -5,3 +5,4 @@ class StoreModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
--- schema.py
+++ schema.py
@@ -1,11 +1,10 @@
 from marshmallow import Schema, fields
 
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
 
 
 class ItemUpdateSchema(Schema):
@@ -13,6 +12,14 @@ class ItemUpdateSchema(Schema):
     price = fields.Float()
 
 
-class StoreSchema(Schema):
+class PlainStoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainItemSchema(), dump_only=True)
+
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainStoreSchema()), dump_only=True)
