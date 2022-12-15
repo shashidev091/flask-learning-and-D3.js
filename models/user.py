5397c4af0ace89fa2bed44aa6a1960919a0664dc
Shashi Bhagat <skujur871@gmail.com>
2022-12-15 16:01:04 +0530

    Started User module and JWT

---
+++ models/user.py
@@ -0,0 +1,14 @@
+from db import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_first_name = db.Column(db.String(80), unique=False, nullable=False)
+    user_middle_name = db.Column(db.String(80), unique=False, nullable=True)
+    user_last_name = db.Column(db.String(80), unique=False, nullable=True)
+    user_email = db.Column(db.String(80), unique=True, nullable=False)
+    user_phone = db.Column(db.String(15), unique=True, nullable=False)
+    user_gender = db.Column(db.String(10), unique=False, nullable=False)
+    user_password = db.Column(db.String(255), unique=False, nullable=True)
